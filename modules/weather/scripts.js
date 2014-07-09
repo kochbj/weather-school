b23d91bda553da11498b785361aff8067ac1f620
Bernard Koch <akathriel@gmail.com>
2014-07-09 17:14:49 -0400

    Chart Appear

--- modules/weather/scripts.js
+++ modules/weather/scripts.js
@@ -200,16 +200,11 @@ function mapXvis ( evt ) {
 		this.settings.displayWidgets[0].settings.container
 			.show()
 			//.css( { 'width':'auto' } )
-			.animate(
+			/*.animate(
 				{ left:'36%' } ,
 				1000 ,
 				'swing' ,
-				function ( ) {
-					//if ( wInstance.settings.displayWidgets[0].highChart ) {
-					//	wInstance.settings.displayWidgets[0].highChart.setSize( $( this ).width( ) , $( this ).height( ) );
-				//	}
-				}
-			);
+			);*/
 		this.settings.displayStatus = 'vis';
 	} else if ( evtType == 'initialize' ) {
 		this.settings.displayStatus = 'map';
