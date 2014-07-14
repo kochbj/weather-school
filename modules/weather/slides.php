83c242d512210a7f50d14bb2387c78b1ba5cddf5
sglassma <sglassma@aaas.org>
2014-07-14 13:11:42 -0400

    Update slides.php

--- modules/weather/slides.php
+++ modules/weather/slides.php
@@ -493,6 +493,15 @@
 	</div>
 	<div class="instructions">
 		<div class="slider">
+			<div>	
+				<p>In this activity, you are going to see what effect being close to a large body of water has on air temperature at a place.</p>
+			<div/>
+			<div>	
+				<p>The map above pinpoints Troms, Norway, which is located on the Norwegian Sea. In this activity you will explore the temperature patterns in Troms and in a location _______________ miles west of Troms inland. Both these locations are highlighted on the map.<p/>
+			<div/>
+			<div>
+				<p>The graph above show the average daily temperatures in Troms and in ____________________ from December 31, 2003 to Dec. 30, 2004.<p/>
+			<div/>
 			<div>
 				<p>Now use the map above to choose two locations that are at different distances from the ocean.</p>
 				<p>Start by locating and then zooming in on the state of California and the city of San Francisco.</p>
