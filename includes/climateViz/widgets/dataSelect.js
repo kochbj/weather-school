384f167ff4eea3ded070c8be7dc195e08768e290
Bernard Koch <akathriel@gmail.com>
2014-09-23 15:26:50 -0400

    this

--- includes/climateViz/widgets/dataSelect.js
+++ includes/climateViz/widgets/dataSelect.js
@@ -1084,6 +1084,7 @@ function syncList (marker) {
 }
 
 function refreshStations ( evt ) {
+	console.log("refreshStations",evt.data,this.markers);
 	// `this` pointing to the execution-time object context, allow closures by assigning `this` to `wInstance`
 	wInstance = this;
 	if ( !evt.data || !evt.data.marker ) { stationBasedDataFetch( false , false , this ); return; }
@@ -1420,7 +1421,7 @@ function stationBasedDataFetchAjax ( evt ) {
 		data     : this.requestQueue[queryID].query ,
 		
 		success : function( servermsg ) {
-			console.log("stationBasedDataFetchAjax",evt,servermsg);
+			console.log("stationBasedDataFetchAjax",evt,servermsg,wInstance.markers);
 			var widgetIndex = parseInt( servermsg.query.widgetIndex , 10 );
 			var mid = servermsg.results[0].mid;
 			var sid = servermsg.results[0].sid;
