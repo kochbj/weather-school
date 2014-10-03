f6e14fe8af5121a037eecd1f4327e49380e16585
Bernard Koch <akathriel@gmail.com>
2014-10-03 15:42:51 -0400

    test

--- includes/climateViz/widgets/dataSelect.js
+++ includes/climateViz/widgets/dataSelect.js
@@ -1255,7 +1255,7 @@ function refreshStations ( evt ) {
 	if ( (!evt.data || !evt.data.marker) ) {
 		//stationBasedDataFetch( false , false , this ); return;
 		for (i in wInstance.markers) {
-			wInstance._callback({type:'user-select-location',data:{marker:wInstance.markers[i]}});
+			if (!canSelectStation) wInstance._callback({type:'user-select-location',data:{marker:wInstance.markers[i]}});
 		}
 		return;
 	}
