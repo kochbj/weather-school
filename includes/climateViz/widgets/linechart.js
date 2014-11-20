e0f544fb51ec43db77a85d452363b78dba637bae
Bernard Koch <akathriel@gmail.com>
2014-11-20 16:50:45 -0500

    test

--- includes/climateViz/widgets/linechart.js
+++ includes/climateViz/widgets/linechart.js
@@ -71,6 +71,7 @@ function createChart (wInstance) {
 				wInstance.chart[chartAxis[idxDataKey]+'Axis'].max=yMax; 
 			}*/
 		 	if (wInstance.data[series].dataMeta[dataKeys[idxDataKey]].range ) {
+				console.log(wInstance.data[series].dataMeta[dataKeys[idxDataKey]].range, "RANGE");	
 				wInstance.chart[chartAxis[idxDataKey]+'Axis'].min = wInstance.data[series].dataMeta[dataKeys[idxDataKey]].range[0];
 				wInstance.chart[chartAxis[idxDataKey]+'Axis'].max = wInstance.data[series].dataMeta[dataKeys[idxDataKey]].range[1];}
 			else {
