a45f3b8aad2c1eccdbf4b7c6a6473100b0e4217a
Bernard Koch <akathriel@gmail.com>
2014-12-17 13:20:06 -0500

    Added standards page

---
+++ pages/standards.php
@@ -0,0 +1,82 @@
+<h2 style="text-align:center; font-size:175%;">Learning Goals from American Education Standards</h2>
+<div style="display:table; margin:3em auto;">
+<a href="http://www.nextgenscience.org" style = "display: table-cell; vertical-align: middle;"><h3 style="font-size:150%; margin: auto;" >Next Generation Science Standards</h2></a>
+<a href="http://www.nextgenscience.org" style = "display: table-cell; vertical-align: middle;"><img src="media/img/ngsslogo.jpg" height="30px" alt="NGSS logo" style="margin-left: 1em;"  /></a>
+</div>
+
+<h4 style="margin:auto; font-size:150%; text-align: center;">NGSS Disciplinary Core Ideas</h4>
+<h5 style="font-size:150%">MS-ESS2.D: Weather and Climate</h5>
+<ul style="font-size:125%">
+<li>Weather and climate are influenced by interactions involving sunlight, the ocean, the atmosphere, <span style="color:lightgray;">ice,</span>* landforms<span style="color:lightgray;">, and living things</span>. These interactions vary with latitude, altitude, and local and regional geography<span style="color:lightgray;">, all of which can affect oceanic and atmospheric flow patterns</span>.</li>
+<span style="color:lightgray;"><li>Because these patterns are so complex, weather can only be predicted probabilistically.</li></span>
+<li>The ocean exerts a major influence on weather and climate by absorbing energy from the sun, releasing it over time<span style="color:lightgray;">, and globally redistributing it through ocean currents</span>. (NGSS Standards, p. 82)</li>
+</ul>
+
+<h4 style="margin:2.5em auto; font-size:150%; text-align: center;">NGSS Scientific Practices</h4>
+<h5 style="font-size:150%">Practice 1: Asking Questions and Defining Problems </h5>
+<p style="font-size:125%">Ask questions</p>
+<ul style="font-size:125%">
+<li>that arise from careful observation of phenomena, models, or unexpected results, to clarify and/or seek additional information.</li>
+<li>to determine relationships between independent and dependent variables and relationship in models. (NGSS Appendixes, p. 51)</li>
+</ul>
+<h5 style="font-size:150%">Practice 3: Planning and Carrying out Investigations</h5>
+<p style="font-size:125%">Collect data <span style="color:lightgray;">to produce data</span> to serve as the basis for evidence to answer scientific questions <span style="color:lightgray;">or to test design solutions under a range of conditions</span>. (NGSS Appendixes, p. 55)</p>
+
+<h5 style="font-size:150%">Practice 4: Analyzing and Interpreting Data</h5> 
+<ul style="font-size:125%">
+<li>Construct, analyze, and/or interpret graphical displays of data and /or large data sets to identify linear and non-linear relationships.</li>
+<li>Use graphical displays (e.g. maps, charts, graphs, and/or tables) of large data sets to identify temporal and spatial relationships.</li>
+<li>Analyze and interpret data to determine similarities and differences in findings. (NGSS Appendixes, p. 57)</li>
+</ul>
+
+<h5 style="font-size:150%">Practice 5: Using Mathematics and Computational Thinking</h5>
+<p style="font-size:125%">Use digital tools (computers) to analyze very large data sets for patterns and trends. (NGSS, Volume 2, Appendixes, p. 59)</p>
+
+<h4 style="margin:2.5em auto; font-size:150%; text-align: center;">NGSS Crosscutting Concepts</h4>
+<h5 style="font-size:150%">Patterns</h5>
+<ul style="font-size:125%">
+<li>Patterns in rates of change and other numerical relationships can provide information about natural systems. (NGSS Standards, p. 81)</li>
+</ul>
+
+<h5 style="font-size:150%">Cause and Effect</h5>
+<ul style="font-size:125%">
+<li>Cause and effect relationships may be used to predict phenomena in natural systems. (NGSS Standards, p. 81)</li>
+Patterns in rates of change and other numerical relationships can provide information about natural systems. (NGSS Standards, p. 81)</li>
+<li>Students] also understand that phenomena may have more than one cause<span style="color:lightgray;">, and some cause and effect relationship in systems can only be described using probability</span>. (NGSS Appendixes, p. 83)</li>
+</ul>
+
+<h5 style="font-size:150%">Scale, Proportion, and Quantity</h5>
+<ul style="font-size:125%">
+<li>Time, space, and energy phenomena can be observed at various scales using models to study systems that are too large <span style="color:lightgray;">or too small</span>. (NGSS Standards, p. 81)</li>
+</ul>
+
+<h5 style="font-size:150%">Stability and Change</h5>
+<ul style="font-size:125%">
+<li>Explanations of stability and change in natural or designed systems can be constructed by examining the changes over time <span style="color:lightgray;">and processes at different scales, including the atomic scale</span>. (NGSS Standards, p. 81)</li>
+</ul>
+<br>
+<hr>
+
+<div style="display:table; margin:3em auto;">
+<a href="http://www.corestandards.org" style = "display: table-cell; vertical-align: middle;"><h3 style="font-size:150%; margin: auto;" >Common Core State Standards in Mathematics: Statistics and Probability</h2></a>
+<a href="http://www.corestandards.org" style = "display: table-cell; vertical-align: middle;"><img src="media/img/ccsslogo.jpg" height="30px" alt="NGSS logo" style="margin-left: 1em;"  /></a>
+</div>
+
+<h5 style="font-size:150%">Investigate patterns of association in bivariate data</h5>
+<ul style="font-size:125%">
+<li>Construct and interpret scatter plots for bivariate measurement data to investigate patterns of association between two quantities. Describe patterns such as <span style="color:lightgray;">clustering, outliers, positive or negative association,</span> linear association, and nonlinear association.</li>
+<li>Know that straight lines are widely used to model relationships between two quantitative variables. For scatter plots that suggest a linear association<span style="color:lightgray;">, informally fit a straight line, and informally assess the model fit by judging the closeness of the data points to the line</span>.  (CCSS-M, p. 56)</li>
+</ul>
+
+<br>
+<hr>
+
+<p style="margin:auto;font-size:125%">*Parts of the NGSS and CCSS-M statements that are not addressed by WeatherSchool are grayed out.</p>
+
+<div style="font-size: 88%; margin: 4em;">
+	<p>Development of this website is supported by grant # NA09SEC4690008 from the U.S. Department of Commerce, National Oceanic and Atmospheric Administration and by grant # NNX09AL72G from the National Aeronautics and Space Administration.</p>
+	<p align="center">
+		<a href="http://www.nasa.gov"><img src="media/img/nasa-226x170.jpg" alt="NASA logo" width="82" height="60" border="0" align="absmiddle" style="margin: 0em 35px;" /></a>
+		<a href="http://www.noaa.gov"><img src="media/img/noaa-220x220.jpg" alt="NOAA logo" width="68" height="68" border="0" align="absmiddle" style="margin: 0em 35px;" /></a>
+	</p>
+</div>
