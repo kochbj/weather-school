cc73218fb66c8799845118583bf2641ef70ff0f4
Brian Sweeney <brian@eclecticgeek.com>
2015-01-09 19:38:06 -0500

    Updating Google Analytics
    Though really this should be set at deployment and not in the repo

--- templates/main.php
+++ templates/main.php
@@ -34,29 +34,13 @@
 	<script type="text/javascript" src="<?php echo CLIMATE_DIR_WWW; ?>/includes/jquery/plugins/jquery.cookie.js"></script>
 	
 	<script>
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-411599-5']);
-		_gaq.push(['_trackPageview']);
-		
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-		
-		$('document').ready(function() {
-			$('a:regex(href, \.(doc|docx*|xls|xlsx*|ppt|pptx*|exe|zip|pdf)$)').click(function () {
-				_gaq.push(['_trackEvent', 'download', 'click', (this.pathname.substring(0,1)=='/'?this.pathname:'/'+this.pathname)]);
-			});
-			$('a[href^="mailto:"]').click(function () {
-				_gaq.push(['_trackEvent', 'email', 'click', this.href.replace(/mailto:/, "")]);
-			});
-			$('a[href^="http\\:\\/\\/"]').click(function () {
-				if (location.hostname != this.href.hostname) {
-					_gaq.push(['_trackEvent', 'outbound', 'click', this.href.replace(/http:\/\//, "")]);
-				}
-			});
-		});
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-4074773-4', 'auto');
+		ga('send', 'pageview');
 	</script>
 
 
